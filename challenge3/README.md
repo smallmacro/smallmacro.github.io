6f0dec6c6533e55ea6d3c81d02787825d85d74c5
smallmacro <smallmacro@gmail.com>
2021-11-08 17:39:48 +0800

    Upload challenge3

---
+++ challenge3/README.md
@@ -0,0 +1,88 @@
+# Frontend Mentor - Time tracking dashboard solution
+
+This is a solution to the [Time tracking dashboard challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/time-tracking-dashboard-UIQ7167Jw). 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Switch between viewing Daily, Weekly, and Monthly stats
+
+- customize a layout for tablet(640px~900px)
+- Load the json data file into UI
+
+### Screenshot
+
+![Mobile screenshot](./mobile_screenshot.png)
+
+![Tablet screenshot](./tablet_screenshot.png)
+
+![Desktop screenshot](./desktop_screenshot.png)
+
+![Interactive features](./interaction.gif)
+
+
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+- basic ES6 
+
+
+
+### What I learned
+1. Safely munipulate the DOM by listening to the `DOMContenLoaded` event.
+
+2. Complete the basic ES6 syntax learning and use the `let` and  `arrow function`
+
+3. Load the json data through `fetch` function.
+
+4. Mobile-first flow works well since there are many similarities among the mobile, tablet(customized by myself) and desktop design.
+
+### Continued development
+If considered the performance of the loading page, is there something needed to be improved?
+
+### Useful resources
+
+- [LICEcap](https://www.cockos.com/licecap/) - Great and simple screen recording app, really recommand it.
+
+
+## Author
+
+- [@smallmacro](https://github.com/smallmacro)
+
+
+
+
+## Acknowledgments
+- [Frontend Mentor](https://www.frontendmentor.io/)
---
+++ challenge3/app/script.js
@@ -0,0 +1,98 @@
+//Global variables
+let timeFrame = 'weekly'; // set the default timefame to weekly
+let url = '../data.json';  //default json url
+let json_data;  
+
+document.addEventListener('DOMContentLoaded',function(){
+    const timeFrameEles = document.querySelectorAll('.timeframe li');
+    
+    
+    fetch(url).then(respone => {
+        return respone.json();
+    }).then(data => {
+        json_data = data;
+        initial(json_data);
+        bindeClickAndHover(timeFrameEles);
+    }).catch((error) => {
+        console.error('Error:', error);
+    });;
+    
+})
+
+
+// load initial state. 
+//when first loading the data, the default statistic is by weekly.
+//data is an array of objects
+function initial(data){
+    console.log(timeFrame);
+    setStatistics(timeFrame,data);
+}
+
+//change timeframe events
+const changeTimeFrame = (clickEle) => {
+    const id = clickEle.getAttribute('id');
+    timeFrame = id;
+    console.log(timeFrame);
+};
+
+
+//set the statstics for all the UI
+const setStatistics = (tFrame, data) => {
+    data.forEach(element => {
+        const title = element.title.toLowerCase();
+        const lower_case_title = title.split(' ').reduce((acc,cur) => {
+            acc = acc + '-' + cur;
+            return acc
+        },);
+        const stat_ele = document.querySelector('#' + lower_case_title);
+        const cur_time = stat_ele.querySelector('.current-time');
+        const prev_time = stat_ele.querySelector('.pre-time');
+        if( tFrame === "monthly"){
+            prev_time.textContent = "Last Month -" + element['timeframes'][tFrame]['previous'] + "hrs";
+        }else if(tFrame === "daily"){
+            prev_time.textContent = "Last Day -" + element['timeframes'][tFrame]['previous'] + "hrs";
+        }else {
+            prev_time.textContent = "Last Week -" + element['timeframes'][tFrame]['previous'] + "hrs";
+        }
+        cur_time.textContent = element['timeframes'][tFrame]['current'] + "hrs"; 
+    });
+
+    
+}
+
+//define the hover and click event
+
+const updateStat = (allEles, currentEle) => {
+    changeTimeFrame(currentEle);
+    allEles.forEach(ele => {
+        if(ele.classList.contains('active')){
+            ele.classList.remove('active');
+        }
+    });
+    currentEle.classList.add('active');
+    setStatistics(timeFrame,json_data);
+};
+
+//Binding events
+const bindeClickAndHover = (elementArr) => {
+    for (let index = 0; index < elementArr.length; index++) {
+        const element = elementArr[index];
+        element.addEventListener('click', (e) => {
+            e.preventDefault();
+            updateStat(elementArr, element);
+        });
+        element.addEventListener('mouseover', (e) => {
+            e.preventDefault();
+            updateStat(elementArr, element);
+        });
+
+    }
+}
+
+// define async fetch function, 
+// return a Promise
+// async function fetch_json(url){
+//     let respone = await fetch(url);
+//     let data = await respone.json();
+//     return data;
+// }
