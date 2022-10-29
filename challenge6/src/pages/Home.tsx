8c27f09746b23b335fdd925b6ef25adf9451dcf7
smallmacro <smallmacro@gmail.com>
2022-10-28 20:56:50 -0400

    Add related types

--- challenge6/src/pages/Home.tsx
+++ challenge6/src/pages/Home.tsx
@@ -1,14 +1,39 @@
-import React from 'react'
+import React, { createContext } from 'react'
 import { useLoaderData } from 'react-router-dom'
 import Banner from '../components/Banner'
 import JobBoard from '../components/JobBoard'
 
 export type CompanyInfo = {
-  
+  company: string;
+  logo: SVGElement;
+  logoBackground: string;
+  position: string;
+  postedAt: string;
+  contract: string;
+  location: string;
 }
+export type Requirement = {
+  content: string;
+  items: Array<string>
+}
+export type Role = {
+  content: string;
+  items: Array<string>
+}
+
+export type JobInfo = {
+  id: number;
+  website: string;
+  apply : string;
+  description:string;
+  requirements: Requirement;
+  role: Role;
+}
+
 const Home = () => {
-  const data = useLoaderData();
-  console.log(data);
+  const data = useLoaderData() as Array<CompanyInfo&JobInfo>| null;
+ 
+  const JobContext = createContext<Array<CompanyInfo&JobInfo> | null>(null);
 
   return (
     <>
@@ -16,8 +41,9 @@ const Home = () => {
     
       <Banner />
     
-    
-      <JobBoard />
+      <JobContext.Provider value={data} >
+        <JobBoard />
+      </JobContext.Provider>
     
 
 
