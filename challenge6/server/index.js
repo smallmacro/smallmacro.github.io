3376cac008d562454f4c8c1ee747bd76b99bcd5b
smallmacro <smallmacro@gmail.com>
2022-11-04 16:30:15 -0400

    Update server configuration

--- challenge6/server/index.js
+++ challenge6/server/index.js
@@ -5,7 +5,7 @@ let jobs = JSON.parse(rawData)
 const express = require('express')
 const app = express()
 const cors = require('cors');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.use(cors())
 app.use(express.json())
 
